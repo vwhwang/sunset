a5c6042668af84793e735beeb6dca9d46c38de62
vwhwang <vwhwang38@gmail.com>
2024-08-13 18:11:43 -0700

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.bignerdranch.android.sunset
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.bignerdranch.android.sunset.ui.theme.SunsetTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+}
