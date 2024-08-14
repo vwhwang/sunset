064e17ee425caccef986ddfa92ffc84d2d1e95f3
vwhwang <vwhwang38@gmail.com>
2024-08-13 18:58:45 -0700

    ch25 set simple sunset animation

--- app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.sunset
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -13,12 +14,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.bignerdranch.android.sunset.databinding.ActivityMainBinding
 import com.bignerdranch.android.sunset.ui.theme.SunsetTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scene.setOnClickListener {
+            startAnimation()
+        }
+    }
+
+    private fun startAnimation() {
+        val sunYStart = binding.sun.top.toFloat()
+        val sunYEnd = binding.sky.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+
+        heightAnimator.start()
     }
 
     companion object {
