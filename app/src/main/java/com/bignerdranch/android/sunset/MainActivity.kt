7a11ecf31213ef023f9aa3cb41e539e2bb7cfe2d
vwhwang <vwhwang38@gmail.com>
2024-08-14 19:50:15 -0700

    ch25 challenge sun rise

--- app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.bignerdranch.android.sunset
 
+import android.animation.AnimatorSet
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.AccelerateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -14,19 +17,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.bignerdranch.android.sunset.databinding.ActivityMainBinding
 import com.bignerdranch.android.sunset.ui.theme.SunsetTheme
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val blueSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.blue_sky)
+    }
+
+    private val sunsetSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.sunset_sky)
+    }
+
+    private val nightSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.night_sky)
+    }
+
+    private var screenClicked = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.scene.setOnClickListener {
-            startAnimation()
+            if (screenClicked) {
+                //reverse Animation
+                sunRiseAnimation()
+                screenClicked = false
+            } else {
+                startAnimation()
+                screenClicked = true
+            }
         }
     }
 
@@ -37,8 +62,49 @@ class MainActivity : AppCompatActivity() {
         val heightAnimator = ObjectAnimator
             .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
             .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
 
-        heightAnimator.start()
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val nightSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", sunsetSkyColor, nightSkyColor)
+            .setDuration(1500)
+        nightSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val animationSet = AnimatorSet()
+        animationSet.play(heightAnimator)
+            .with(sunsetSkyAnimator)
+            .before(nightSkyAnimator)
+        animationSet.start()
+    }
+
+    private fun sunRiseAnimation() {
+        val sunYStart = binding.sky.bottom.toFloat()
+        val sunYEnd = binding.sun.top.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
+
+        val sunriseSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", nightSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+        sunriseSkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val daySkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", sunsetSkyColor, blueSkyColor)
+            .setDuration(1500)
+        daySkyAnimator.setEvaluator(ArgbEvaluator())
+
+        val animationSet = AnimatorSet()
+        animationSet.play(heightAnimator)
+            .with(sunriseSkyAnimator)
+            .before(daySkyAnimator)
+        animationSet.start()
     }
 
     companion object {
